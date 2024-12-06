879778f43cab6f37b3109dec87b6ec0f4abccd81
Antonia  Ayala <50365103+antoniaayala96@users.noreply.github.com>
2024-12-05 19:56:37 -0600

    cc

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBWLCDKgNHjfa5XSFXWmU55kAVnTwbYv0c",
+  authDomain: "com.company.foro",
+  projectId: "foro-2-e9cb0",
+  storageBucket: "foro-2-e9cb0.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:android:abcdefgh123456"
 };
 
 const app = initializeApp(firebaseConfig);
